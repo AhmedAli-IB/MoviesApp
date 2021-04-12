610de101d87ec375d83b18ebd7d3f445f4b0848d
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-04-12 14:47:10 +0200

    Add dummy unit tests

--- MoviesAppUITests/MoviesAppUITests.swift
+++ MoviesAppUITests/MoviesAppUITests.swift
@@ -9,34 +9,32 @@ import XCTest
 
 class MoviesAppUITests: XCTestCase {
 
+    // MARK: - Properteis
+    //
+    let firstNumber = 10
+    let  secoundNumber = 5
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAdd()  {
+        XCTAssertEqual( firstNumber + secoundNumber, 15)
     }
+    
+    func testSubtract()  {
+        XCTAssertEqual( firstNumber - secoundNumber, 5)
+    }
+    
+    func testMultiply()  {
+        XCTAssertEqual( firstNumber * secoundNumber, 50)
+    }
+    
+    func testDivide() {
+        XCTAssertEqual( firstNumber / secoundNumber, 2)
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
